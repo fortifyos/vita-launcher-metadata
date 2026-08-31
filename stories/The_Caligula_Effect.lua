3be9bd0d35a8b3ec61daec00cfad09888d04e9a8
Pierre Gustave <fortifylabs@icloud.com>
2026-08-30 20:23:07 -0400

    Add detailed Vita Game Story entries

---
+++ stories/The_Caligula_Effect.lua
@@ -0,0 +1,8 @@
+return {
+    genre = "Role-playing",
+    year = "2017",
+    players = "1 Player",
+    synopsis = "A modern RPG about students living in an artificial paradise who begin to realize that escaping it may be more painful than staying.",
+    gameplay = "Battles use a turn-based timeline that previews possible actions, letting you plan chains before committing. Recruiting classmates and learning their hidden problems adds a social-investigation layer to the dungeon progression.",
+    ideal_for = "Players interested in a psychologically themed Japanese RPG with an experimental battle system and a large cast."
+  }
