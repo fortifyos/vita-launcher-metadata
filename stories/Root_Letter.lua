00b32f1ea781ad18fd25d0c7d035174253f305a4
Pierre Gustave <fortifylabs@icloud.com>
2026-08-30 20:26:47 -0400

    Add detailed Vita Game Story entries

---
+++ stories/Root_Letter.lua
@@ -0,0 +1,8 @@
+return {
+    genre = "Visual novel Adventure",
+    year = "2016",
+    players = "1 Player",
+    synopsis = "A letter-driven mystery about a man returning to a Japanese town to investigate the fate of a pen pal who vanished years earlier.",
+    gameplay = "You question former classmates, inspect locations, and choose how forcefully to pursue a lead. Different choices change which version of the past you uncover, encouraging revisits after the first ending.",
+    ideal_for = "Players looking for a grounded mystery visual novel with investigation scenes and several possible conclusions."
+  }
