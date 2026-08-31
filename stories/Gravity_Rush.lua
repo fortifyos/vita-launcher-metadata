2f574ebba1fc47fe6e4edf198d47fd9a38a52ddf
Pierre Gustave <fortifylabs@icloud.com>
2026-08-30 20:14:16 -0400

    Add curated Vita Game Story entries

---
+++ stories/Gravity_Rush.lua
@@ -0,0 +1,8 @@
+return {
+    genre = "Action-adventure",
+    year = "2012",
+    players = "1 Player",
+    synopsis = "A comic-book-styled adventure about a heroine who can redirect gravity and turn an entire floating city into her route forward.",
+    gameplay = "You shift gravity to fall in any direction, then combine aerial movement with light combat and mission-based exploration. Learning to orient yourself is part of the fun.",
+    ideal_for = "Players looking for an imaginative Vita-exclusive concept with movement that feels unlike a standard action game."
+  }
